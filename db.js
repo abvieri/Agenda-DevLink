ada46ce10ba7c4703fa3857a1b061993ba568010
Abraão Vieri <127057422+abvieri@users.noreply.github.com>
2025-03-18 19:24:49 -0300

    Update db.js

--- db.js
+++ db.js
@@ -4,11 +4,11 @@ const mysql = require('mysql2/promise');
 async function getConnection() {
   try {
     const connection = await mysql.createPool({
-      host: 'localhost',
-      user: 'root',
-      password: '123456',
-      database: 'agenda_contato',
-      port: 3307,
+      host: 'bbnddvnxc2azi22dbplj-mysql.services.clever-cloud.com',
+      user: 'ugq9nwc4xowcemy7',
+      password: 'bO64FEsqBx1PjZjDpLbX',
+      database: 'bbnddvnxc2azi22dbplj',
+      port: 3306,
       waitForConnections: true,
       connectionLimit: 10,
       queueLimit: 0
