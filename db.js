0da0d14309a15916a729ff6aff12e1850196d129
Abraão Vieri <127057422+abvieri@users.noreply.github.com>
2025-03-22 17:04:29 -0300

    Update db.js

--- db.js
+++ db.js
@@ -1,21 +1,26 @@
-const mysql = require('mysql2/promise');
-
-// Cria o pool de conexões uma vez
-const pool = mysql.createPool({
-  host: 'bbnddvnxc2azi22dbplj-mysql.services.clever-cloud.com',
-  user: 'ugq9nwc4xowcemy7',
-  password: 'bO64FEsqBx1PjZjDpLbX',
-  database: 'bbnddvnxc2azi22dbplj',
-  port: 3306,
-  waitForConnections: true,
-  connectionLimit: 5, // Ajuste o limite conforme permitido (limite atual: 5)
-  queueLimit: 0
-});
-
-pool.getConnection()
-  .then(() => console.log('✅ Conectado ao banco de dados MySQL!'))
-  .catch(err => {
-    console.error('❌ Erro ao conectar ao banco de dados:', err);
-  });
-
-module.exports = pool;
+require('dotenv').config();
+ const mysql = require('mysql2/promise');
+ 
+ async function getConnection() {
+   try {
+     const connection = await mysql.createPool({
+       host: 'bbnddvnxc2azi22dbplj-mysql.services.clever-cloud.com',
+       user: 'ugq9nwc4xowcemy7',
+       password: 'bO64FEsqBx1PjZjDpLbX',
+       database: 'bbnddvnxc2azi22dbplj',
+       port: 3306,
+       waitForConnections: true,
+       connectionLimit: 10,
+       queueLimit: 0
+     });
+ 
+     console.log('✅ Conectado ao banco de dados MySQL!');
+     return connection;
+   } catch (err) {
+     console.error('❌ Erro ao conectar ao banco de dados:', err);
+     throw err;
+   }
+ }
+ 
+ // Exporta a função para obter a conexão
+ module.exports = getConnection;
