0e350a7f4bff6d929bf195d236cc7da8b94c616e
Abraão Vieri <127057422+abvieri@users.noreply.github.com>
2025-03-22 16:28:36 -0300

    Remove process.env db.js

--- db.js
+++ db.js
@@ -1,12 +1,11 @@
-require('dotenv').config();
 const mysql = require('mysql2/promise');
 
 // Cria o pool de conexões uma vez
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || 'bbnddvnxc2azi22dbplj-mysql.services.clever-cloud.com',
-  user: process.env.DB_USER || 'ugq9nwc4xowcemy7',
-  password: process.env.DB_PASSWORD || 'bO64FEsqBx1PjZjDpLbX',
-  database: process.env.DB_NAME || 'bbnddvnxc2azi22dbplj',
+  host: 'bbnddvnxc2azi22dbplj-mysql.services.clever-cloud.com',
+  user: 'ugq9nwc4xowcemy7',
+  password: 'bO64FEsqBx1PjZjDpLbX',
+  database: 'bbnddvnxc2azi22dbplj',
   port: 3306,
   waitForConnections: true,
   connectionLimit: 5, // Ajuste o limite conforme permitido (limite atual: 5)
